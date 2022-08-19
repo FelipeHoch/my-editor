23e069d705bb3d72ff5ee989e13aebf419957f86
Felipe Hoch <felipe.hoch@sirros.net>
2022-08-19 14:14:25 -0300

    Adds plugin to generate package.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgxWigModule } from 'ngx-wig';
 
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { EditorComponent } from './editor/editor.component';
   ],
   imports: [
     BrowserModule,
-    NgxWigModule
+    NgxWigModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
